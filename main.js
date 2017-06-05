892d55a5281b7ee920d8e71e15459f99d215e9ca
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-06-05 11:03:16 -0400

    cleaned files

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
 $(document).ready(function() {
-	
-	$(window).on('resize', function () {
-	
+
+	$(window).on('resize', function() {
+
 		var height = $(window).height() - 20;
-	//	var width = $(window).width();
-		
+		//	var width = $(window).width();
+
 		$('.container').css('width', height);
 		$('.container').css('height', height);
-	   
+
 	});
-	
+
 	$(window).resize();
-	
-	
+
 	$('.square').on('click', function() {
-		
+
 		$(this).find('.content-front').toggle();
 		$(this).find('.content-back').toggle();
-		
+
 	})
-	
-	
+
 	// Click on vote buttons
 	$('.btn-vote').on('click', function() {
-		
+
 		if ($(this).hasClass('btn-agree')) {
-				//#19b162
-			$(this).closest('.square').animate({backgroundColor : '#109e54'}, 1000, function() {
+			//#19b162
+			$(this).closest('.square').animate({
+				backgroundColor : '#109e54'
+			}, 1000, function() {
 				console.log('animation done')
 			});
-	
+
 		} else {
-			
-			$(this).closest('.square').animate({backgroundColor : '#d85c4a'}, 1000, function() {
+
+			$(this).closest('.square').animate({
+				backgroundColor : '#d85c4a'
+			}, 1000, function() {
 				console.log('animation done')
 			});
 		}
-		
+
 	});
-	
+
 });
