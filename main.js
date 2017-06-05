d0ff87024f3297f55dc99b840f5357aef54dc47c
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-06-05 11:02:38 -0400

    cleaned index.html

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
-$(window).on('resize', function () {
-
-	var height = $(window).height() - 20;
-//	var width = $(window).width();
+$(document).ready(function() {
 	
-	$('.container').css('width', height);
-	$('.container').css('height', height);
-   
-});
-
-$(window).resize();
-
-
-$('.square').on('click', function() {
+	$(window).on('resize', function () {
 	
-	$(this).find('.content-front').toggle();
-	$(this).find('.content-back').toggle();
-	
-})
-
-
-// Click on vote buttons
-$('.btn-vote').on('click', function() {
-	
-	if ($(this).hasClass('btn-agree')) {
-			//#19b162
-		$(this).closest('.square').animate({backgroundColor : '#109e54'}, 1000, function() {
-			console.log('animation done')
-		});
-
-	} else {
+		var height = $(window).height() - 20;
+	//	var width = $(window).width();
 		
-		$(this).closest('.square').animate({backgroundColor : '#d85c4a'}, 1000, function() {
-			console.log('animation done')
-		});
-	}
+		$('.container').css('width', height);
+		$('.container').css('height', height);
+	   
+	});
+	
+	$(window).resize();
+	
+	
+	$('.square').on('click', function() {
+		
+		$(this).find('.content-front').toggle();
+		$(this).find('.content-back').toggle();
+		
+	})
+	
+	
+	// Click on vote buttons
+	$('.btn-vote').on('click', function() {
+		
+		if ($(this).hasClass('btn-agree')) {
+				//#19b162
+			$(this).closest('.square').animate({backgroundColor : '#109e54'}, 1000, function() {
+				console.log('animation done')
+			});
+	
+		} else {
+			
+			$(this).closest('.square').animate({backgroundColor : '#d85c4a'}, 1000, function() {
+				console.log('animation done')
+			});
+		}
+		
+	});
 	
 });
-
-
-
