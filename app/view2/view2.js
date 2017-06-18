c4af4a03177a75ce91b3b14b8135aa836b06fb5f
Lauren ten Hoor <lauren.ten.hoor@philips.com>
2017-06-18 23:06:18 +0200

    Added a draft static model for news items

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,27 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
-
+.controller('View2Ctrl', ['$scope', function($scope) {
+	$scope.newsItems = [{
+		title : 'Het klimaat akkoord',
+		articleUrl : '',
+		imgUrl : '',
+		logoUrl : '',
+		summaryText : '',
+		type : 'Read'
+	}, {
+		title : '0.3',
+		articleUrl : '',
+		imgUrl : '',
+		logoUrl : '',
+		summaryText : '',
+		type : 'Read'
+	}, {
+		title : '',
+		articleUrl : '',
+		imgUrl : '',
+		logoUrl : '',
+		summaryText : '',
+		type : 'Read'
+	}];
 }]);
