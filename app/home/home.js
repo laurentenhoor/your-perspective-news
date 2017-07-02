5793ef0cdd83d1b5e76e01bd5e94b59d79d1b425
Lauren ten Hoor <lauren.ten.hoor@philips.com>
2017-07-02 12:08:05 +0200

    Added a design folder for graphical design documents

--- app/home/home.js
+++ app/home/home.js
@@ -13,10 +13,36 @@ angular.module('myApp.home', ['ngRoute'])
 	
 	$scope.newsItems = [{
 		id : 1,
-		title: 'VS stapt uit klimaatakkoord'
+		title: 'VS stapt uit klimaatakkoord',
+		category: 'buitenland'
 	}, {
 		id : 2,
-		title: 'Asha gaat naar Madeira'
+		title: 'Crisis Qatar',
+		category: 'buitenland'
+	}, {
+		id : 3,
+		title: 'Verkiezingen Frankrijk',
+		category: 'buitenland'
+	}, {
+		id : 4,
+		title: 'Formatieonderhandelingen',
+		category: 'politiek'
+	}, {
+		id : 5,
+		title: 'Nederland belastingparadijs',
+		category: 'economie'
+	}, {
+		id : 6,
+		title: 'Overlijden Savannah',
+		category: 'binnenland'
+	}, {
+		id : 7,
+		title: 'Oranje op WK',
+		category: 'sport'
+	}, {
+		id : 8,
+		title: 'Beschermingsconstructie AkzoNobel',
+		category: 'economie'
 	}];
 	
 
