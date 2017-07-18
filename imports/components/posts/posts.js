fcab675a262e9e77d5d73449733f1b40ad2f9962
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-07-18 22:57:59 +0200

    directive for http in url input field

--- imports/components/posts/posts.js
+++ imports/components/posts/posts.js
@@ -70,4 +70,25 @@ export default angular.module('allpers.post', [
 	.component('allpersPost', {
 		templateUrl : template,
 		controller: ['$scope', '$http', PostCtrl]
+	})
+	.directive('httpPrefix', function() {
+	    return {
+	        restrict: 'A',
+	        require: 'ngModel',
+	        link: function(scope, element, attrs, controller) {
+	            function ensureHttpPrefix(value) {
+	                // Need to add prefix if we don't have http:// prefix already AND we don't have part of it
+	                if(value && !/^(https?):\/\//i.test(value)
+	                   && 'http://'.indexOf(value) !== 0 && 'https://'.indexOf(value) !== 0 ) {
+	                    controller.$setViewValue('http://' + value);
+	                    controller.$render();
+	                    return 'http://' + value;
+	                }
+	                else
+	                    return value;
+	            }
+	            controller.$formatters.push(ensureHttpPrefix);
+	            controller.$parsers.splice(0, 0, ensureHttpPrefix);
+	        }
+	    };
 	});
