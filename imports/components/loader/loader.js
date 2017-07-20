8c2ad45f3bddbd4adbc2c58d70d106f0b03a3531
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-07-21 01:24:48 +0200

    Solved back button problem for loader

--- imports/components/loader/loader.js
+++ imports/components/loader/loader.js
@@ -5,37 +5,67 @@ import template from './loader.html';
 import style from './loader.less';
 
 class LoaderCtrl {
-	
-  constructor($rootScope, $scope, $location) {
-	  
-	  $rootScope.stateIsLoading = true;
-	  
-	  $rootScope.$on( "$routeChangeStart", function(event, next, current) {
-		  $rootScope.stateIsLoading = true;
+
+	constructor($rootScope, $scope, $location) {
+
+		$rootScope.stateIsLoading = true;
+
+		$rootScope.$on( "$routeChangeStart", function(event, next, current) {
+			console.log('$routeChangeStart');
+//			alert('start')
+			$rootScope.stateIsLoading = true;
 		});
-	  
-	  $rootScope.$on( "$routeChangeSuccess", function(event, next, current) {
-		  $rootScope.stateIsLoading = false;
+
+		$rootScope.$on( "$routeChangeSuccess", function(event, next, current) {
+			console.log('$routeChangeSuccess');
+//			alert('success')
+			$rootScope.stateIsLoading = false;
 		});
-  }
+
+		$rootScope.$on('$locationChangeStart',function(evt, absNewUrl, absOldUrl) {
+			console.log('start', evt, absNewUrl, absOldUrl);
+			$rootScope.stateIsLoading = false;
+
+		});
+
+		$rootScope.$on('$locationChangeSuccess',function(evt, absNewUrl, absOldUrl) {
+			console.log('success', evt, absNewUrl, absOldUrl);
+			$rootScope.stateIsLoading = false;
+
+		});
+
+		// Hide the loader after back-button-press on mobile devices
+		// https://stackoverflow.com/questions/34270818/how-can-i-hide-a-loading-overlay-when-user-returns-to-page-via-back-button
+		if ("onpagehide" in window) {
+			window.addEventListener("pageshow", function() {
+				setTimeout(function(){ 
+					$rootScope.stateIsLoading = false;
+					$scope.$apply(); 
+				}, 50);
+			}, false);
+		}
+
+	}
+
+
 
 }
- 
+
 export default angular.module('allpers.loader', [
-  angularMeteor
-])
-  .component('allpersLoader', {
-    templateUrl : template,
-    controller: ['$rootScope', '$scope', '$location', LoaderCtrl]
-  })
-  
-.directive('extLink', ['$rootScope', function($rootScope) {
-  return {
-    restrict: 'A',
-    link: function(scope, elem) {
-      elem.bind('click', function(e) {
-    	  	$rootScope.stateIsLoading = true;  
-      });
-    }
-  };
-}]);
+	angularMeteor
+	])
+	.component('allpersLoader', {
+		templateUrl : template,
+		controller: ['$rootScope', '$scope', '$location', LoaderCtrl]
+	})
+
+	.directive('extLink', ['$rootScope', function($rootScope) {
+		return {
+			restrict: 'A',
+			link: function(scope, elem) {
+				elem.bind('click', function(e) {
+					$rootScope.stateIsLoading = true;  
+				});
+			}
+		};
+	}]);
