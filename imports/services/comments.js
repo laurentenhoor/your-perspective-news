aa21560b18dc2f1ed6b06ff0cf691996902a3ef3
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-10-09 19:42:59 +0200

    Clean code update for hierarchy builder

--- imports/services/comments.js
+++ imports/services/comments.js
@@ -10,29 +10,11 @@ class CommentSortingService {
   buildCommentHierarchy(unsortedComments) {
 	  
 	    var topLevelComments = findTopLevelComments(unsortedComments);
-	    var responsesPerCommentId = findResponsesPerCommentId(unsortedComments);
-
-
-	    // function to recursively build the tree
-	    var addResponsesToSelectedComment = function(selectedComment) {
-	        
-	    	if (responsesPerCommentId[selectedComment._id]) {
-	        	
-	        	// add responsesPerCommentId to selectedComment and sort by voting score
-	            selectedComment.children = responsesPerCommentId[selectedComment._id].sort(function(a, b){
-	            		return (b.score - a.score);
-	            });
-	            
-	            for (var i = 0, amountOfComments = selectedComment.children.length; i < amountOfComments; ++i) {
-	                addResponsesToSelectedComment(selectedComment.children[i]);
-	            }
-	            
-	        }
-	    };
+	    var repliesPerCommentId = findRepliesPerCommentId(unsortedComments);
 
 	    // enumerate through to handle the case where there are multiple topLevelComments
 	    for (var i = 0, amountOfComments = topLevelComments.length; i < amountOfComments; ++i) {
-	        addResponsesToSelectedComment(topLevelComments[i]);
+	    		recursivelyAddRepliesToSelectedTopLevelComment(topLevelComments[i], repliesPerCommentId);
 	    }
 
 	    // sort root by voting score
@@ -49,7 +31,7 @@ class CommentSortingService {
 function findTopLevelComments(unsortedComments) {
 
 	  	var topLevelComments = [];
-	    // find the top level nodes and hash the responsesPerCommentId based on parent
+	    // find the top level nodes and hash the repliesPerCommentId based on parent
 	    for (var i = 0, amountOfComments = unsortedComments.length; i < amountOfComments; ++i) {
 	    	
 	    	var selectedComment = unsortedComments[i];
@@ -63,27 +45,47 @@ function findTopLevelComments(unsortedComments) {
 	    return topLevelComments;
 }
 
-function findResponsesPerCommentId(unsortedComments) {
+
+function findRepliesPerCommentId(unsortedComments) {
 	  
-	  	var responsesPerCommentId = {};
-	    // find the top level nodes and hash the responsesPerCommentId based on parent
+	  	var repliesPerCommentId = {};
+	  	
+	    // find the top level nodes and hash the repliesPerCommentId based on parent
 	    for (var i = 0, amountOfComments = unsortedComments.length; i < amountOfComments; ++i) {
 	    	
 	    	var selectedComment = unsortedComments[i];
 	    	
 	      	if (unsortedComments[i].parentCommentId) {
 	      		
-	      		if (!responsesPerCommentId[selectedComment.parentCommentId]) {
-      			responsesPerCommentId[selectedComment.parentCommentId] = [];	
+	      		if (!repliesPerCommentId[selectedComment.parentCommentId]) {
+      			repliesPerCommentId[selectedComment.parentCommentId] = [];	
       		}
-      		responsesPerCommentId[selectedComment.parentCommentId].push(selectedComment);
+      		repliesPerCommentId[selectedComment.parentCommentId].push(selectedComment);
 	      		
 	      	} 
 
 	    }
-	    return responsesPerCommentId;
+	    return repliesPerCommentId;
 	  
 }
+
+
+//function to recursively build the tree
+function recursivelyAddRepliesToSelectedTopLevelComment(selectedTopLevelComment, repliesPerCommentId) {
+ 
+	if (repliesPerCommentId[selectedTopLevelComment._id]) {
+ 	
+ 	// add repliesPerCommentId to selectedTopLevelComment and sort by voting score
+     selectedTopLevelComment.children = repliesPerCommentId[selectedTopLevelComment._id].sort(function(a, b){
+     		return (b.score - a.score);
+     });
+     
+     for (var i = 0, amountOfComments = selectedTopLevelComment.children.length; i < amountOfComments; ++i) {
+    	 	recursivelyAddRepliesToSelectedTopLevelComment(selectedTopLevelComment.children[i], repliesPerCommentId);
+     }
+     
+ }
+};
 
 // create a module
 export default angular.module('yourpers.selectedCommentSortingService', [
