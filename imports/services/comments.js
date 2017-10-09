69c289a386497214a7e3b2c7b2d99ddf73bd61ce
310172618 <310172618@YY098753.code1.emi.philips.com>
2017-10-09 19:28:12 +0200

    Clean code transformation of comment hierarchy.

--- imports/services/comments.js
+++ imports/services/comments.js
@@ -6,46 +6,25 @@ class CommentSortingService {
   constructor() {
     
   }
-  
+    
   buildCommentHierarchy(unsortedComments) {
+	  
+	    var topLevelComments = findTopLevelComments(unsortedComments);
+	    var responsesPerCommentId = findResponsesPerCommentId(unsortedComments);
 
-	    var topLevelComments = [];
-	    var responsesPerCommentId = {};
-
-	    // find the top level nodes and hash the responsesPerCommentId based on parent
-	    for (var i = 0, amountOfComments = unsortedComments.length; i < amountOfComments; ++i) {
-	    	
-	        var selectedComment = unsortedComments[i];
-//	            parentCommentId = selectedComment.parentCommentId
-//	            target = !parentCommentId ? topLevelComments : (responsesPerCommentId[parentCommentId] || (responsesPerCommentId[parentCommentId] = []));
-        
-        	if (!selectedComment.parentCommentId) {
-        		
-        		topLevelComments.push(selectedComment);
-        		
-        	} else {
-        		
-        		if (!responsesPerCommentId[selectedComment.parentCommentId]) {
-        			responsesPerCommentId[selectedComment.parentCommentId] = [];	
-        		}
-        		responsesPerCommentId[selectedComment.parentCommentId].push(selectedComment);
-        		
-        	}
-
-//	        target.push(selectedComment);
-	    }
 
 	    // function to recursively build the tree
-	    var findAllChildrenOfSelectedComment = function(selectedComment) {
-	        if (responsesPerCommentId[selectedComment._id]) {
+	    var addResponsesToSelectedComment = function(selectedComment) {
+	        
+	    	if (responsesPerCommentId[selectedComment._id]) {
 	        	
-	        		// add responsesPerCommentId to selectedComment and sort by voting score
+	        	// add responsesPerCommentId to selectedComment and sort by voting score
 	            selectedComment.children = responsesPerCommentId[selectedComment._id].sort(function(a, b){
 	            		return (b.score - a.score);
 	            });
 	            
 	            for (var i = 0, amountOfComments = selectedComment.children.length; i < amountOfComments; ++i) {
-	                findAllChildrenOfSelectedComment(selectedComment.children[i]);
+	                addResponsesToSelectedComment(selectedComment.children[i]);
 	            }
 	            
 	        }
@@ -53,7 +32,7 @@ class CommentSortingService {
 
 	    // enumerate through to handle the case where there are multiple topLevelComments
 	    for (var i = 0, amountOfComments = topLevelComments.length; i < amountOfComments; ++i) {
-	        findAllChildrenOfSelectedComment(topLevelComments[i]);
+	        addResponsesToSelectedComment(topLevelComments[i]);
 	    }
 
 	    // sort root by voting score
@@ -63,6 +42,47 @@ class CommentSortingService {
 	    
 	    return topLevelComments;
 	}
+}
+
+
+
+function findTopLevelComments(unsortedComments) {
+
+	  	var topLevelComments = [];
+	    // find the top level nodes and hash the responsesPerCommentId based on parent
+	    for (var i = 0, amountOfComments = unsortedComments.length; i < amountOfComments; ++i) {
+	    	
+	    	var selectedComment = unsortedComments[i];
+	    	 
+	      	if (!unsortedComments[i].parentCommentId) {
+	      		topLevelComments.push(selectedComment);
+	      		
+	      	} 
+
+	    }
+	    return topLevelComments;
+}
+
+function findResponsesPerCommentId(unsortedComments) {
+	  
+	  	var responsesPerCommentId = {};
+	    // find the top level nodes and hash the responsesPerCommentId based on parent
+	    for (var i = 0, amountOfComments = unsortedComments.length; i < amountOfComments; ++i) {
+	    	
+	    	var selectedComment = unsortedComments[i];
+	    	
+	      	if (unsortedComments[i].parentCommentId) {
+	      		
+	      		if (!responsesPerCommentId[selectedComment.parentCommentId]) {
+      			responsesPerCommentId[selectedComment.parentCommentId] = [];	
+      		}
+      		responsesPerCommentId[selectedComment.parentCommentId].push(selectedComment);
+	      		
+	      	} 
+
+	    }
+	    return responsesPerCommentId;
+	  
 }
 
 // create a module
