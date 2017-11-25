80d396e37284f270301f27b24f1d0945ad3ca9c1
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-11-25 12:25:11 +0100

    Added database migrations

--- server/migrations/migrations.js
+++ server/migrations/migrations.js
@@ -3,7 +3,7 @@ import { Topics } from '../../imports/api/topics.js';
 if (Meteor.isServer) {
   Migrations.add({
     version: 1,
-    name: 'its a test',
+    name: 'Initial database',
     up: function() {console.log('Inside migrations');}
   });
  
@@ -12,31 +12,34 @@ if (Meteor.isServer) {
 	    version: 2,
 	    name: 'add articleIds array to topics',
 	    up: function() {
+
+	    	_.forEach(Topics.find({}).fetch(), function(topic){
+	    		
+	    		_.forEach(topic.articlesByCategory, function(category, i) {
+
+	    			if ('articleIds' in category) {
+	    				console.log('this is a modern schema')
+	    				return;
+	    			}
+
+	    			var articleIds = [];		
+	    			_.forEach(category.articles, function(article) {			
+	    				articleIds.push(article._id);
+	    			})
+	    			topic.articlesByCategory[i].articleIds = articleIds;
+	    			
+	    		});
+	    		//update full scheme
+	    		Topics.update({_id:topic._id}, topic);
+	    		
+	    	});
 	    	
-//	    	Topics.
-	    	
-	    	console.log('Inside migrations');}
+	    }
 	  });
  
 
   Meteor.startup(function () {
     // code to run on server at startup
-    Migrations.migrateTo(1);
+    Migrations.migrateTo(2);
   });
 }
-
-
-topics = Topics.find({_id: 'b7zQZf4ZkRxh9rJkz'}).fetch();
-console.log(topics);
-
-_.forEach(topics, function(topic){
-    //process item
-//	console.log(topic);
-	
-	_.forEach(topic.articlesByCategory, function(category) {
-		
-		console.log(category._id)
-		
-		
-	})
-});
