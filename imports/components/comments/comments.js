b2d09163b4f568b0a492424f0505711117122661
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-11-30 19:34:39 +0100

    Initial working commments module

--- imports/components/comments/comments.js
+++ imports/components/comments/comments.js
@@ -1,18 +1,23 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
+import {name as CommentsTreeBuilder} from '../../services/CommentsTreeBuilder.js';
+
 import template from './comments.html';
 import style from './comments.less';
+import commentsTreeTemplate from './commentsTree.html';
 
 import { Comments } from '../../api/comments.js';
 
 
 class CommentsCtrl {
 
-	constructor($rootScope, $scope, $reactive) {
+	constructor($rootScope, $scope, $reactive, CommentsTreeBuilder) {
 
 		var $ctrl = this;
 		$reactive($ctrl).attach($scope);
+		
+		$ctrl.commentsTreeTemplate = commentsTreeTemplate;
 		
 		Meteor.subscribe('comments', {
 			onReady: function(){
@@ -23,16 +28,43 @@ class CommentsCtrl {
 						var comments = Comments.find({parentItemId: $ctrl.getReactively('topicId')})
 						console.log('fetch commments for topicId: '+ $ctrl.topicId); 
 						
-//						var roots = CommentsTreeBuilder.getCommentsTree(comments.fetch());
-//						console.log(roots);
+						var roots = CommentsTreeBuilder.getCommentsTree(comments.fetch());
+						console.log(roots);
 					
-//						return roots;
+						return roots;
 					}
 
 				});
 		
 			}
 		});
+	
+		
+		$ctrl.newComment = function(comment) {
+			
+			console.log(comment.newChildComment);
+			console.log(comment._id);
+
+			Comments.insert({
+				
+				parentItemId : $ctrl.topicId,
+				parentCommentId : comment._id,
+				ownerId : Meteor.userId(),
+				ownerName : Meteor.user() ? Meteor.user().username : null,
+				comment : comment.newChildComment,
+				score: 0
+				
+			}, function(error, _id){
+				
+				console.log('error: ' + error);
+				console.log('_id: ' + _id);
+				
+				comment.newChildComment = '';
+				$scope.$apply();
+
+			});
+			
+		}
 				
 		$ctrl.click = function() {
 			console.log($ctrl.topicId);
@@ -46,10 +78,11 @@ class CommentsCtrl {
 
 export default angular.module('yourpers.comments', [
 	angularMeteor,
+	CommentsTreeBuilder,
 	])
 	.component('yourpersComments', {
 		templateUrl : template,
-		controller: ['$rootScope', '$scope', '$reactive', CommentsCtrl],
+		controller: ['$rootScope', '$scope', '$reactive', 'CommentsTreeBuilder', CommentsCtrl],
 		bindings: {
 			topicId : '<',
 		}
