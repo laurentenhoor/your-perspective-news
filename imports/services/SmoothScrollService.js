e2667dd631029ea3df62be6c775068e4c4c58794
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-12-02 11:24:27 +0100

    Improved smooth scroll

--- imports/services/SmoothScrollService.js
+++ imports/services/SmoothScrollService.js
@@ -9,85 +9,36 @@ class SmoothScrollService {
 	}
 
 	horizontalScroll(targetId, scrollScopeId) {
-
-        // This scrolling function 
-        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
-
-		var myElement = document.getElementById(targetId);
-		console.log(targetId);
-		var leftPos = myElement.offsetLeft;
-		console.log(leftPos);
+		
 		var scrollElement = document.getElementById(scrollScopeId);
-		console.log(scrollElement);
+		var targetElement = document.getElementById(targetId);
 		
-//		scrollElement.scrollLeft = leftPos;
+		var startX = scrollElement.scrollLeft;
+		var stopX = targetElement.offsetLeft;
+		var distance = stopX > startX ? stopX - startX : startX - stopX;
 		
+		if (distance < 100) {
+			scrollElement.scrollLeft = stopX;  
+			return;
+		}
 		
-      var startY = scrollElement.scrollLeft;
-      var stopY = myElement.offsetLeft;
-      var distance = stopY > startY ? stopY - startY : startY - stopY;
-      if (distance < 100) {
-          scrollTo(0, stopY); return;
-      }
-      var speed = Math.round(distance / 100);
-      if (speed >= 20) speed = 20;
-      var step = Math.round(distance / 25);
-      var leapY = stopY > startY ? startY + step : startY - step;
-      var timer = 0;
-      if (stopY > startY) {
-          for ( var i=startY; i<stopY; i+=step ) {
-              setTimeout("document.getElementById('"+scrollScopeId+"').scrollLeft = "+leapY, timer * speed);
-              leapY += step; if (leapY > stopY) leapY = stopY; timer++;
-          } return;
-      }
-      for ( var i=startY; i>stopY; i-=step ) {
-          setTimeout("document.getElementById("+scrollScopeId+").scrollLeft = "+leapY, timer * speed);
-          leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
-      }	
+		var speed = Math.round(distance / 200);
+		if (speed >= 20) speed = 20;
+		var step = Math.round(distance / 25);
+		var leapX = stopX > startX ? startX + step : startX - step;
+		var timer = 0;
 		
+		if (stopX > startX) {
+			for ( var i=startX; i<stopX; i+=step ) {
+				setTimeout("document.getElementById('"+scrollScopeId+"').scrollLeft = "+leapX, timer * speed);
+				leapX += step; if (leapX > stopX) leapX = stopX; timer++;
+			} return;
+		}
 		
-//        var startY = currentYPosition();
-//        var stopY = elmYPosition(eID);
-//        var distance = stopY > startY ? stopY - startY : startY - stopY;
-//        if (distance < 100) {
-//            scrollTo(0, stopY); return;
-//        }
-//        var speed = Math.round(distance / 100);
-//        if (speed >= 20) speed = 20;
-//        var step = Math.round(distance / 25);
-//        var leapY = stopY > startY ? startY + step : startY - step;
-//        var timer = 0;
-//        if (stopY > startY) {
-//            for ( var i=startY; i<stopY; i+=step ) {
-//                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-//                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
-//            } return;
-//        }
-//        for ( var i=startY; i>stopY; i-=step ) {
-//            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-//            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
-//        }
-//        
-//        function currentYPosition() {
-//            // Firefox, Chrome, Opera, Safari
-//            if (self.pageYOffset) return self.pageYOffset;
-//            // Internet Explorer 6 - standards mode
-//            if (document.documentElement && document.documentElement.scrollTop)
-//                return document.documentElement.scrollTop;
-//            // Internet Explorer 6, 7 and 8
-//            if (document.body.scrollTop) return document.body.scrollTop;
-//            return 0;
-//        }
-//        
-//        function elmYPosition(eID) {
-//            var elm = document.getElementById(eID);
-//            var y = elm.offsetTop;
-//            var node = elm;
-//            while (node.offsetParent && node.offsetParent != document.body) {
-//                node = node.offsetParent;
-//                y += node.offsetTop;
-//            } return y;
-//        }
+		for ( var i=startX; i>stopX; i-=step ) {
+			setTimeout("document.getElementById("+scrollScopeId+").scrollLeft = "+leapX, timer * speed);
+			leapX -= step; if (leapX < stopX) leapX = stopX; timer++;
+		}	
 
 	}
 }
