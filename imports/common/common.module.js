6f3db7ad0c00bb9536f7b6a04ab116ae24161753
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-12-29 15:42:25 +0700

    minor refactor

--- imports/common/common.module.js
+++ imports/common/common.module.js
@@ -1,18 +1,17 @@
-import headerMenu from './header-menu/header-menu';
-import topics from './topics/topics.module';
-import articleMenu from './article-menu/article-menu';
-import desktopViewer from './desktop-viewer/desktop-viewer';
-import firstUseToast from './first-use-toast/fist-use-toast.module';
+import HeaderMenu from './header-menu/header-menu';
+import Topics from './topics/topics.module';
+import ArticleMenu from './article-menu/article-menu';
+import DesktopViewer from './desktop-viewer/desktop-viewer';
+import FirstUseToast from './first-use-toast/fist-use-toast.module';
 import FistUseDialog from './first-use-dialog/first-use-dialog.module';
 
-export default
-    angular.module('app.common', [
-
-        desktopViewer,
-        headerMenu,
-        topics,
-        articleMenu,
-        firstUseToast,
+export default angular
+    .module('app.common', [
+        DesktopViewer,
+        HeaderMenu,
+        Topics,
+        ArticleMenu,
+        FirstUseToast,
         FistUseDialog,
-
-    ]).name;
+    ])
+    .name;
