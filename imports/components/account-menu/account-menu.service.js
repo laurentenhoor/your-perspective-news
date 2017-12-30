879d8a629805da47a2b09391e2b7f1ce78ab8da1
Lauren ten Hoor <laurentenhoor@gmail.com>
2017-12-30 16:57:31 +0700

    Improved style

--- imports/components/account-menu/account-menu.service.js
+++ imports/components/account-menu/account-menu.service.js
@@ -1,3 +1,5 @@
+import angular from 'angular';
+
 export default class AccountMenuService {
 
 	static create($mdSidenav){
@@ -8,11 +10,22 @@ export default class AccountMenuService {
 	constructor($mdSidenav) {
 		'ngInject';
 
+		let isOpen = false;
+
 		return {
 			toggle: toggle
 		}
 
 		function toggle() {
+			isOpen = !isOpen;
+
+			var bodyElement = angular.element(document.querySelector('body'));
+			if (isOpen) {
+				bodyElement.addClass('md-dialog-is-showing');
+			} else {
+				bodyElement.removeClass('md-dialog-is-showing');
+			}
+			
 			return $mdSidenav('account-menu').toggle();
 		}
 	}
