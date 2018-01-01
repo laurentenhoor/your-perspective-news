77d122c77e159866e2c20358638b79e1d74e5020
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-01-01 05:17:59 +0100

    Deprecated backend collections

--- server/main.js
+++ server/main.js
@@ -1,7 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { HTTP } from 'meteor/http'
 
-import '/imports/api/posts.js';
 import '/imports/api/votes.js';
 import '/imports/api/feedback.js';
 import '/imports/api/comments.js';
