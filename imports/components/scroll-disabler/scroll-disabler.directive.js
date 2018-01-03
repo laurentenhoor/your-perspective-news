4f60ce05ffff34e685dd810e4d3036c53c9669a7
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-01-03 22:37:38 +0100

    Finalized scroll-disabler

--- imports/components/scroll-disabler/scroll-disabler.directive.js
+++ imports/components/scroll-disabler/scroll-disabler.directive.js
@@ -6,25 +6,16 @@ export default class ScrollDisablerDirective {
 		this.restrict = 'A';
 		this.link = function ($scope, $element, $attrs) {
 
-			$scope.$watch(() => {
+			$scope.$watch(() => $scrollDisabler.isScrollingDisabled(),
+				(scrollingDisabled) => {
 
-				return $scrollDisabler.isScrollingDisabled()
+					if (scrollingDisabled) {
+						$element.addClass('scrolling-disabled');
+					} else {
+						$element.removeClass('scrolling-disabled');
+					}
 
-			}, (scrollingDisabled) => {
-
-				if (scrollingDisabled) {
-
-					$element.addClass('scrolling-disabled')
-					console.log('disabled scrolling from directive')
-
-				} else {
-
-					$element.removeClass('scrolling-disabled')
-					console.log('enabled scrolling from directive')
-
-				}
-				
-			});
+				});
 
 		}
 
