552bbd6698622fc804f5e41001ce3fd0e4588790
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-01-03 21:01:52 +0100

    improved adding references and scroll padding

--- imports/common/write-opinion/rich-text-editor/rich-text-editor.component.js
+++ imports/common/write-opinion/rich-text-editor/rich-text-editor.component.js
@@ -28,9 +28,7 @@ class RichTextEditorComponent {
             if (changes.addArticle && $ctrl.addArticle) {
                 let article = angular.copy($ctrl.addArticle);
                 let htmlString = $sanitize('<a class="source-chip" href="' + article.url + '">&nbsp;' + (article.publisher || (article.title.substring(0, 9) + '...') || 'link') + '&nbsp;</a>​');
-                editor.removeLink();
                 editor.insertHTML(htmlString);
-                editor.removeLink();
             }
         }
 
