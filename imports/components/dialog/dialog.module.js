ba34918d0a8ca19ed809420f903946014116131c
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-01-05 10:47:20 +0100

    Added global dialog style and refactored stylesheets

--- imports/components/dialog/dialog.module.js
+++ imports/components/dialog/dialog.module.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import angularMaterial from 'angular-material';
 
 import DialogService from './dialog.service';
+import DialogStyle from'./dialog.styl';
 
 export default angular
     .module('Dialog', ['ScrollDisabler', angularMaterial])
