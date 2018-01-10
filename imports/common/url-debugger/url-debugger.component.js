2ec64a7bfbf779453e38149a450f05a318b66740
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-01-10 09:08:49 +0100

    Crawler update

--- imports/common/url-debugger/url-debugger.component.js
+++ imports/common/url-debugger/url-debugger.component.js
@@ -45,11 +45,12 @@ class UrlDebuggerComponent {
 
         }
 
-        $ctrl.url = 'https://fd.nl/economie-politiek/1210447/overheidsbalans-lagere-gasbaten-maken-nederland-in-een-klap-60-miljard-euro-armer';
+        // $ctrl.url = 'https://fd.nl/economie-politiek/1210447/overheidsbalans-lagere-gasbaten-maken-nederland-in-een-klap-60-miljard-euro-armer';
         // $ctrl.url = 'https://www.nytimes.com/2018/01/09/sports/alabama-national-championship.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=photo-spot-region&region=top-news&WT.nav=top-news';
         // $ctrl.url = 'https://www.google.nl/amp/s/www.volkskrant.nl/binnenland/uitspraak-college-politie-discrimineert-met-verbod-op-hoofddoek-in-niet-publieke-functie~a4540290/amp';
         // $ctrl.url = 'https://www.ad.nl/buitenland/triomf-for-trump-belastingplan-is-erdoor~a8bd0881/';
         // $ctrl.url = 'https://dekanttekening.nl/samenleving/poolse-nederlanders-vinden-polen-niet-xenofobisch/'
+        // $ctrl.url = 'http://www.economist.com/blogs/dailychart/2010/11/cartography?fsrc=scn/fb/te/pe/ed/truesizeafrica';
 
         $ctrl.processUrl();
 
