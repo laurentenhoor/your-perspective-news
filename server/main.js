00e970674e52ebdb8dd961bffb188effe71d9c25
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-01-13 23:50:58 +0100

    Userin Jimp for graphics

--- server/main.js
+++ server/main.js
@@ -6,6 +6,8 @@ import '/imports/api/feedback.js';
 import '/imports/api/comments.js';
 import '/imports/api/opinions.js';
 
+var Jimp = require('jimp');
+
 if (!Package.appcache)
 	WebApp.connectHandlers.use(function (req, res, next) {
 		var params = req.url.split('/');
@@ -18,27 +20,49 @@ if (!Package.appcache)
 		console.log('valid share url for topic:', itemId)
 
 		if (Inject.appUrl(req.url)) {
-			Inject.rawHead('myData', setMetaTagesDynamically(), res);
+			Inject.rawHead('myData', setMetaTagesDynamically(itemId), res);
 		}
 		next();
 
 	});
 
+
 Router.route('/i/:text.png', function () {
 
 	console.log('ironrouter fired')
 
-	var imgContent = Meteor.call('getImage', this.params.text);
-	imgContent = new Buffer(imgContent, 'binary');
+	var self = this;
+	var url = "http://img.humo.be/q100/w1200/h628/img_178/1789097.jpg"
 
-	this.response.writeHeader('200', {
-		'Content-Type': 'image/png',
-		'Content-Length': imgContent.length
+	Jimp.read(url, function (error, image) {
+
+		if (error) {
+			return console.error(error);;
+		}
+
+		image.cover(600, 600);
+
+		Jimp.loadFont(Jimp.FONT_SANS_128_BLACK)
+			.then((font) => {
+
+				image.print(font, 20, 300, self.params.text)
+				// image.color([{ apply: 'darken', params: [30] }])
+
+				image.getBuffer(Jimp.AUTO, (error, imageBuffer) => {
+
+					self.response.writeHeader('200', {
+						'Content-Type': 'image/png',
+						'Content-Length': imageBuffer.length
+					});
+
+					self.response.write(imageBuffer);
+					self.response.end();
+
+				});
+			});
+
 	});
 
-	this.response.write(imgContent);
-
-	this.response.end();
 }, { where: 'server' });
 
 Meteor.startup(() => {
@@ -67,10 +91,12 @@ Meteor.startup(() => {
 
 });
 
-function setMetaTagesDynamically() {
+function setMetaTagesDynamically(itemId) {
 	var tags = `
 		<meta property="og:title" content="jouwpers open graph">
-		<meta property="og:image" content="http://wij.jouwpers.nl/i/jouwpers%20server%20open%20graph.png">
+		<meta property="og:image" content="http://wij.jouwpers.nl/i/`+ itemId + `.png">
+		<meta property="og:image:width" content="600">
+		<meta property="og:image:height" content="600">
 		`
 	return tags;
 }
