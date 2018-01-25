9277caff501c66dc022acf0e8d32e4a2b019e084
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-01-25 19:53:31 +0100

    Renaming url preprocessing

--- server/imports/metascraper/url-preprocessing.js
+++ server/imports/metascraper/url-preprocessing.js
@@ -1,36 +1,35 @@
 var google = require('google')
 
+function googlePromise(query) {
+    return new Promise(
+        (resolve, reject) => {
+            google(query, (error, result) => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(result.links[0].href);
+                }
+            })
+
+        })
+}
+
 module.exports = async (url) => {
 
-    var urlParts = url.split("/");
+    var thisAIsBlendleUrl = false;
     var searchIndex = -1;
+    var urlParts = url.split("/");
+
     _.each(urlParts, (part, index) => {
         if (part.indexOf('blendle.com') > -1) {
             searchIndex = index;
+            thisIsABlendleUrl = true;
         }
     })
 
-    function googlePromise(query) {
-        return new Promise(
-            (resolve, reject) => {
-                google(query, (error, result) => {
-                    if (error) {
-                        reject(error)
-                    } else {
-                        resolve(result.links[0].href);
-                    }
-                })
-
-            })
-    }
-
-    if (searchIndex > -1) {
-
+    if (thisIsABlendleUrl) {
         let query = urlParts[searchIndex + 2] + ' ' + urlParts[searchIndex + 3];
-        console.log('search query', query);
-
         return await googlePromise(query);
-
     } else {
         return url;
     }
