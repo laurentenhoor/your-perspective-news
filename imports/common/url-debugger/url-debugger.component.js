ccdf95c6556ce4407f0152bc23ff2c838a738dab
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-01-25 13:03:44 +0100

    Added problem descriptions to bad urls

--- imports/common/url-debugger/url-debugger.component.js
+++ imports/common/url-debugger/url-debugger.component.js
@@ -11,29 +11,38 @@ class UrlDebuggerComponent {
         $loader.databaseInitialized();
         
         $ctrl.urls = [            
+            
+            // Does load on local, but not in production
             'https://www.google.nl/amp/s/nos.nl/googleamp/artikel/129340-de-financiele-crisis-in-retroperspectief.html',
+            
+            // Loads nothing at all
+            'https://goo.gl/images/bzni4a',
+
+            // No image!
+            'https://www.nu.nl/brexit/5082617/harde-brexit-kost-britten-bijna-half-miljoen-banen.amp',
+
+            // Low resolution logo
+            'https://www.vpro.nl/programmas/tegenlicht/kijk/afleveringen/2013-2014/de-noodzaak-van-een-utopie.html',
+
+            // Passed the test!
             'https://www.youtube.com/watch?v=4XDWtU1Zojw',
             'http://www.dumpert.nl/mediabase/7341269/bce179d5/code_rood.html',
             'http://www.dumpert.nl/mediabase/7347615/0e8baec2/meiske_doet_schieten.html',
-            
             'https://fd.nl/economie-politiek/1210447/overheidsbalans-lagere-gasbaten-maken-nederland-in-een-klap-60-miljard-euro-armer',
             'https://www.nytimes.com/2018/01/09/sports/alabama-national-championship.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=photo-spot-region&region=top-news&WT.nav=top-news',
             'https://www.google.nl/amp/s/www.volkskrant.nl/binnenland/uitspraak-college-politie-discrimineert-met-verbod-op-hoofddoek-in-niet-publieke-functie~a4540290/amp',
             'https://www.ad.nl/buitenland/triomf-for-trump-belastingplan-is-erdoor~a8bd0881/',
             'https://dekanttekening.nl/samenleving/poolse-nederlanders-vinden-polen-niet-xenofobisch/',
             'http://www.economist.com/blogs/dailychart/2010/11/cartography?fsrc=scn/fb/te/pe/ed/truesizeafrica',
-            'https://www.vpro.nl/programmas/tegenlicht/kijk/afleveringen/2013-2014/de-noodzaak-van-een-utopie.html',
             'https://www.businessinsider.nl/tesla-heeft-een-nieuwe-versie-van-zijn-roadster-sportwagen-gepresenteerd-en-het-echt-een-game-changer/',
             'https://nl.wikipedia.org/wiki/Koninklijke_Philips',
             'https://blendle.com/i/de-standaard/me-too-in-nederland/bnl-destandaard-20180108-eee62560_f250_11e7_bfa5_c2ed917c3dd8?sharer=eyJ2ZXJzaW9uIjoiMSIsInVpZCI6ImJhcnRkaW5namFuIiwiaXRlbV9pZCI6ImJubC1kZXN0YW5kYWFyZC0yMDE4MDEwOC1lZWU2MjU2MF9mMjUwXzExZTdfYmZhNV9jMmVkOTE3YzNkZDgifQ%3D%3D',
             'https://www.economist.com/blogs/graphicdetail/2017/05/daily-chart-22',
-            'https://goo.gl/images/bzni4a',
             'https://fd.nl/economie-politiek/1092391/alcohol-en-tabak-zijn-winstmachines-maar-hoe-lang-nog',
-            'https://www.nu.nl/brexit/5082617/harde-brexit-kost-britten-bijna-half-miljoen-banen.amp',
             'https://www.volkskrant.nl/opinie/hetzelfde-ritueel-zal-zich-na-elke-beving-herhalen-tot-het-gas-echt-op-is~a4555873/',
             'https://www.bnr.nl/podcast/bernard-hammelburg/10336697/opstand-in-iran',
             'https://nl.m.wikipedia.org/wiki/Iraanse_Revolutie',
-            'https://fd.nl/beurs/1235376/aanpak-corruptie-is-het-grote-thema-in-latijns-amerika',
+            'https://fd.nl/beurs/1235376/aanpak-corruptie-is-het-grote-thema-in-latijns-amerika'
            
         ]
 
