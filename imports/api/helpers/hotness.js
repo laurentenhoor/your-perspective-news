3b1841be79c59d925d6af46d933ee37a93830f72
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-01-27 16:24:51 +0100

    Cleaned hotness module

--- imports/api/helpers/hotness.js
+++ imports/api/helpers/hotness.js
@@ -1,11 +1,24 @@
-const hotness = (score, date) => {
+export default hotness = (score, date) => {
+    
+    let order = Math.log(Math.max(Math.abs(score), 1.5), 10)
+    let sign = score > 0 ? 1 : score < 0 ? -1 : 0;
+
+    let youpersFoundingDate = new Date(2017, 3, 1).getTime() / 1000;
+    let articleCreationDate = new Date(date).getTime() / 1000;
+    let seconds = articleCreationDate - youpersFoundingDate;
+
+    let timeWindow = seconds / 45000;
+
+    let hotness = (sign * order + timeWindow)
+
+
+    let precision = 10000000;
+    let roundedHotness = Math.round(hotness * precision) / precision;
 
     // console.clear();
     console.log('')
     console.log('==NEW HOTNESS CALC==');
 
-    let order = Math.log(Math.max(Math.abs(score), 1.5), 10)
-    let sign = score > 0 ? 1 : score < 0 ? -1 : 0;
     console.log('score', score)
 
     console.log("Math.abs(score)", Math.abs(score));
@@ -15,9 +28,6 @@ const hotness = (score, date) => {
     console.log('order', order)
     console.log('sign', sign)
 
-    let youpersFoundingDate = new Date(2017, 3, 1).getTime() / 1000;
-    let articleCreationDate = new Date(date).getTime() / 1000;
-    let seconds = articleCreationDate - youpersFoundingDate;
     console.log('1 jan 2018', youpersFoundingDate)
     console.log('articleCreationDate', articleCreationDate)
     console.log('seconds', seconds)
@@ -25,15 +35,9 @@ const hotness = (score, date) => {
     console.log('sign * order + seconds', sign * order + seconds)
     console.log('sign * order + seconds / 45000', sign * order + seconds / 45000)
 
-    let timeWindow = seconds / 45000;
-
-    let hotness = (sign * order + timeWindow)
     console.log('hotness', hotness)
-
-    let precision = 10000000;
-    let roundedHotness = Math.round(hotness * precision) / precision
     console.log('roundedHotness', roundedHotness)
-    return roundedHotness;
-}
 
-module.exports = hotness;
+    return roundedHotness;
+};
+
