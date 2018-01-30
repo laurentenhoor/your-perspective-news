c9fa1d4ed99b7627e67262034f44373a4992ae79
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-01-30 18:34:43 +0100

    Added analytics script (only in production mode)

---
+++ server/analytics.js
@@ -0,0 +1,17 @@
+let gaInit = `
+<script>Dummy Google Analytics: this will be replaced by the actual tags in production mode</script>
+`
+if (Meteor.isProduction) {
+    gaInit = `
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112278933-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'UA-112278933-1');
+    </script>
+    `;
+}
+Inject.rawHead('Inject the Google Analytics script at the beginning of the head', gaInit);
