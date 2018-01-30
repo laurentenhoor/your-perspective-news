1900141632632e5d6461cc5498ee0c3e726939cd
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-01-30 23:49:37 +0100

    Updated slack message

--- imports/api/users.js
+++ imports/api/users.js
@@ -9,24 +9,26 @@ if (Meteor.isServer) {
     Slack = new SlackAPI('https://hooks.slack.com/services/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');
 
     Accounts.onCreateUser((options, user) => {
-    
-      let newUser = Object.assign({}, user, options)
 
-      if (newUser && newUser.profile && newUser.firstName) {
-        let firstName = newUser.profile.firstName;
-        let lastName = newUser.profile.lastName;
-        let email = newUser.profile.emailAddress;
-        let profileImage = newUser.profile.pictureUrl;
-  
-        Slack.send({
-          text: firstName + ' ' + lastName + ' <'+email+'>', 
-          username: 'New User Registration',
-          icon_url: profileImage
-        });
-      }
-    
-      return newUser;
-      
+        let amountOfUsers = Meteor.users.find({}).count() + 1;
+
+        let newUser = Object.assign({}, user, options);
+
+        if (newUser && newUser.profile && newUser.profile.firstName) {
+            let firstName = newUser.profile.firstName;
+            let lastName = newUser.profile.lastName;
+            let email = newUser.profile.emailAddress;
+            let profileImage = newUser.profile.pictureUrl;
+
+            Slack.send({
+                username: firstName + ' ' + lastName,
+                text: 'Ik heb me zojuist geregistreerd op jouwpers! Jullie hebben nu ' + amountOfUsers + ' leden.',
+                icon_url: profileImage
+            });
+        }
+
+        return newUser;
+
     })
 
     Meteor.publish('allUsernames', function () {
