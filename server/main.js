24698958749b0fc5a0717c7c147a381bcef61367
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-02-12 22:03:35 +0100

    Tested vote edit/delete

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ import { HTTP } from 'meteor/http'
 
 import { Topics } from '/imports/api/topics'
 import { Articles } from '/imports/api/articles'
-// import { Votes } from '/imports/api/votes'
+import { Votes } from '/imports/api/votes'
 import { Comments } from '/imports/api/comments'
 import { Feedback } from '/imports/api/feedback'
 import { Opinions } from '/imports/api/opinions'
