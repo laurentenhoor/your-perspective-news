b6e8a7c1d05c8c10954c69e277a957ba896414f6
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-02-17 16:47:09 +0100

    Improved auth service

--- imports/components/auth/auth.service.js
+++ imports/components/auth/auth.service.js
@@ -11,57 +11,59 @@ export default class AuthService {
 		'ngInject';
 
 		return {
-			login: (callback) => {
-				
-				Meteor.loginWithLinkedIn({
-					loginStyle: ($desktopViewer.isDesktop() ? 'popup' : 'redirect')
-				}, (error) => {
-					if (error) { console.error(error); }
-					callback()
-				});
+			login: login,
+			logout: logout,
+			isLoggedIn: isLoggedIn
+		}
 
-			},
-			logout: () => {
+		function login(callback) {
+			Meteor.loginWithLinkedIn({
+				loginStyle: ($desktopViewer.isDesktop() ? 'popup' : 'redirect')
+			}, (error) => {
+				if (error) { console.error(error); }
+				callback()
+			});
+		}
 
-				ga('send', {
-					hitType: 'event',
-					eventCategory: 'Account',
-					eventAction: 'logout',
-					eventLabel: '/user/' + Meteor.userId()
-				})
+		function logout() {
 
-				$loader.startAndWait(() =>
-					Accounts.logout(() =>
-						$loader.stop()))
+			ga('send', {
+				hitType: 'event',
+				eventCategory: 'Account',
+				eventAction: 'logout',
+				eventLabel: '/user/' + Meteor.userId()
+			})
 
-			},
-			isLoggedIn: () => {
-
-				if (!Meteor.user()) {
-
-					var confirmModal = $dialog.confirm()
-						.title('Log in met LinkedIn')
-						.textContent(`Wij streven naar eerlijke discussies tussen echte mensen.`)
-						.ariaLabel('Log in met LinkedIn')
-						.ok('Login')
-						.cancel('Annuleren')
-
-					$dialog.show(confirmModal).then(function () {
-						login();
-						return;
-					}, function () {
-						console.log('canceled login modal');
-					});
-
-					return false;
-				} else {
-					return true;
-				}
-			}
+			$loader.startAndWait(() =>
+				Accounts.logout(() =>
+					$loader.stop()))
 
 		}
 
+		function isLoggedIn() {
+			if (!Meteor.user()) {
+
+				var confirmModal = $dialog.confirm()
+					.title('Log in met LinkedIn')
+					.textContent(`Wij streven naar eerlijke discussies tussen echte mensen.`)
+					.ariaLabel('Log in met LinkedIn')
+					.ok('Login')
+					.cancel('Annuleren')
+
+				$dialog.show(confirmModal).then(function () {
+					login();
+					return;
+				}, function () {
+					console.log('canceled login modal');
+				});
+
+				return false;
+			} else {
+				return true;
+			}
+		}
 	}
 
-
 }
+
+
